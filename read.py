8e506ac14f257a1214228b5d0843101f2509a9b5
YiChen <spi791017@gmail.com>
2020-08-10 17:23:04 +0800

    calc avg of length

--- read.py
+++ read.py
@@ -1,13 +1,16 @@
 data = []
 count = 0
+sum = 0 
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+for i in data:
+	sum += len(i)
+avg = sum / len(data)
+print('留言平均長度為', avg)
+
 
 #print(data)
-print(data[0])
-print('-'*20)
