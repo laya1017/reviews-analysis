eb679c18000418e2b4a8c88f6a1ca8a2b931b7cd
YiChen <spi791017@gmail.com>
2020-08-10 17:33:23 +0800

    screen <100 sentence

--- read.py
+++ read.py
@@ -11,6 +11,12 @@ for i in data:
 	sum += len(i)
 avg = sum / len(data)
 print('留言平均長度為', avg)
+new = []
+for d in data :
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(new), '留言小於100個字')
+
 
 
 #print(data)
